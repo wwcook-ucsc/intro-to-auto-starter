688cc049e9209a7d60eb1e5568525295287e8219
Weston Cook <wwcook@ucsc.edu>
2021-10-22 21:28:02 +0000

    Update lab3

--- lab3/scripts/safety_node.py
+++ lab3/scripts/safety_node.py
@@ -23,7 +23,10 @@ class Safety(object):
         The subscribers should use the provided odom_callback and scan_callback as callback methods
         NOTE that the x component of the linear velocity in odom is the speed
         """
-        print('emergency braking system is running')
+        rospy.init_node('safety_node')
+
+        rospy.on_shutdown(self.shutdown)
+
         self.ttc_threshold = ttc_threshold
 
         # set up message data variables
@@ -31,6 +34,8 @@ class Safety(object):
         self.ranges = None
         self.angle_min = None
         self.angle_increment = None
+        self.range_min = None
+        self.range_max = None
 
         # create ROS subscribers and publishers.
         self.brake_msg = AckermannDriveStamped()
@@ -43,24 +48,39 @@ class Safety(object):
 
         rate = rospy.Rate(10)
 
+        rospy.loginfo('emergency braking system is running')
+
         while not rospy.is_shutdown():
+            # just make sure the /brake topic is warm
+            # might not be necessary but better safe than crashed
             self.brake_pub.publish(self.brake_msg)
+
             if self.velocity is not None and self.ranges is not None:
                 xvel, yvel = self.velocity['x'], self.velocity['y']
                 ranges = list(self.ranges)
                 angle_min = self.angle_min
                 angle_increment = self.angle_increment
+                self.velocity = None
+                self.ranges = None
                 for i, distance in enumerate(ranges):
+                    # make sure distance is a valid range
+                    if math.isinf(distance) or math.isnan(distance):
+                        continue
+                    if distance < self.range_min:
+                        # choose to brake when the sensor says something is
+                        # invalidly close
+                        self.brake()
+                    distance = min(distance, self.range_max)
+                    # project velocity onto range angle
                     angle = angle_min + i * angle_increment
                     proj_vel = xvel * cos(angle) + yvel * sin(angle)
                     if proj_vel <= 0.0:
                         continue
+
                     ttc = distance / proj_vel
                     if ttc < self.ttc_threshold:
 #                        print('TTC {} at angle {}'.format(ttc, angle * 180. / math.pi))
                         self.brake()
-            self.velocity = None
-            self.ranges = None
             rate.sleep()
 
     def odom_callback(self, odom_msg):
@@ -75,13 +95,23 @@ class Safety(object):
         self.ranges = list(scan_msg.ranges)
         self.angle_min = scan_msg.angle_min
         self.angle_increment = scan_msg.angle_increment
+        self.range_min = scan_msg.range_min
+        self.range_max = scan_msg.range_max
     
     def brake(self):
         # publish brake message
+        rospy.loginfo('BRAKE!!')
         self.brake_bool_pub.publish(self.brake_bool_msg)
+    
+    def shutdown(self):
+        rospy.loginfo('emergency braking system is shutting down')
 
 
 if __name__ == '__main__':
-    rospy.init_node('safety_node')
-    safety_node = Safety(ttc_threshold=0.5)
-    rospy.spin()
+    try:
+        Safety(ttc_threshold=0.3)
+    except Exception as e:
+        rospy.logerr('emergency braking system is terminating')
+        rospy.logerr(e)
+    except:
+        rospy.logerr('emergency braking system is terminating')
